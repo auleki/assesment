af906a3863a0f916827ba23bb96fb793e4c16e83
auleki <kodagiwa@gmail.com>
2020-08-27 04:42:54 +0100

    dark mode added

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Card = () => {
-    return (      
-        <div className="card full dark">
-          <div className="stats">
-            <h3>POS FAILURE RATE</h3>
+  const [isDark, setIsDark] = useState(false);
 
+
+
+  const switchMode = () => {
+    
+  }
+
+  const darkToggle = () => {
+    setIsDark(!isDark)
+  }
+  const pickClass = isDark ? 'card dark full' : 'card light full';
+  
+  return (      
+        <div className={pickClass}>
+          <div className="stats">
+            <h3 className="title">POS FAILURE RATE</h3>
+            {/* FIX STRUCTURE TO CONTAIN TITLE IN A BOX */}
             <div className="info">
               <p className="percentage">60%</p>
               <span className="change"><ion-icon name="caret-up-outline"></ion-icon> 8%</span>
@@ -14,12 +27,31 @@ const Card = () => {
           </div>
           <div className="controls">
             <div className="actions">
-              <div className="mode">MODE</div>
-              <div className="switch">SWITCH</div>
+              <div className="mode">
+                <button
+                  onClick={switchMode}
+                  >
+                  MODE
+                </button>
+              </div>
+              <div className="switch">
+              <button
+                  onClick={darkToggle}
+                  >
+                  SWITCH
+                </button>
+              </div>
             </div>
             
+            <div className="slider">
+              <input type="range" min="1" max="100" value="50"/>
+            </div>
             
             <p className="source">Source <span>NIBBS</span></p>
+          </div>
+
+          <div className="range_minimal">
+            <div className="range_width"></div>
           </div>
 
           <div className="intervals">
